b0e05ec2d1b9af710fbee6b239936a66030a9219
xXCarlosXx08 <130647173+xXCarlosXx08@users.noreply.github.com>
2023-04-17 08:45:45 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,6 +26,31 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(1000)
     }
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # # # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # # #
+            `)
+        basic.pause(1000)
+    }
 })
